da410a961329f829dfeb6dca280c8f40d16c2512
FSRenan <renan.santos@linkconsulting.com.br>
2018-10-30 17:51:28 -0300

    add-method-updateDBPartitions

--- src/DBServer/DBController.java
+++ src/DBServer/DBController.java
@@ -23,9 +23,15 @@ public class DBController {
     static Socket socker_send_partition;
     static Socket client_socket;
 
-    private static final int getPartitionsInfo = 0;
-    private static final int updatePartitionsInfo = 1;
-    private static FileManager fileManager = new FileManager();
+    private static final FileManager fileManager = new FileManager();
+
+    //Constraints
+    private static final int GET_PARTITIONS_INFO = 0;
+    private static final int UPDATE_PARTITIONS_INFO = 1;
+
+    private static final int PORT_PARTITION1 = 9700;
+    private static final int PORT_PARTITION2 = 9800;
+    private static final int PORT_PARTITION3 = 9900;
 
     public DBController() {
         try {
@@ -37,7 +43,6 @@ public class DBController {
             //PARTICOES NOVA
             //socker_send_partition2 = new Socket("localhost", 9800);
             //socker_send_partition3 = new Socket("localhost", 9900);
-
         } catch (IOException ex) {
             System.err.println("*DBServer: Creating server failed >> ERROR: " + ex);
             ex.printStackTrace();
@@ -54,7 +59,6 @@ public class DBController {
         Get get = new Get();
 
         //checkDBPartitions();
-
         while (connect()) {
             //Receive Client request
             post = (Post) Connect.receive(client_socket);
@@ -66,17 +70,13 @@ public class DBController {
             dbActions.execute(post, get);
 
             //Update Partitions
-
-
+            updateDBPartitions(post.getTable());    
+            
+            //Delete Controller Files Buffer
+            
             //Setting Return
             Connect.send(client_socket, get);
 
-//            try {
-//                client_socket.close();
-//                controller_socker_receive.close();
-//            } catch (IOException ex) {
-//                System.out.println("*BDServer: Socket close failed >> ERROR: " + ex);
-//            }
         }
     }
 
@@ -99,22 +99,24 @@ public class DBController {
         boolean searchPartition3 = false;
 
         //PARTITION 1
-        get = getPartitionInfo(9700, table, persons);
-        if (get.getStatus() == 0)
+        get = sendPartitionInfo(GET_PARTITIONS_INFO, PORT_PARTITION1, table, persons);
+        if (get.getStatus() == 0) {
             persons = get.getPersons();
-        else
+        } else {
             searchPartition3 = true;
+        }
 
         //PARTITION 2
-        get = getPartitionInfo(9800, table, persons);
-        if (get.getStatus() == 0)
+        get = sendPartitionInfo(GET_PARTITIONS_INFO, PORT_PARTITION2, table, persons);
+        if (get.getStatus() == 0) {
             persons = get.getPersons();
-        else
+        } else {
             searchPartition3 = true;
+        }
 
         //PARTITION 3
         if (searchPartition3) {
-            get = getPartitionInfo(9900, table, persons);
+            get = sendPartitionInfo(GET_PARTITIONS_INFO, PORT_PARTITION3, table, persons);
             persons = get.getPersons();
         }
 
@@ -122,15 +124,32 @@ public class DBController {
         fileManager.writePersons(persons, table);
     }
 
-    public static DBPartitionCommandGet getPartitionInfo(int port, String table, ArrayList<Person> persons) throws IOException {
+    public static void updateDBPartitions(String table) throws IOException {
+        ArrayList<Person> persons = fileManager.getPersons(table);
+        DBPartitionCommandGet get;
+
+        //PARTITION 1
+        get = sendPartitionInfo(UPDATE_PARTITIONS_INFO, PORT_PARTITION1, table, persons);
+        //PARTITION 2
+        get = sendPartitionInfo(UPDATE_PARTITIONS_INFO, PORT_PARTITION2, table, persons);
+        //PARTITION 3
+        get = sendPartitionInfo(UPDATE_PARTITIONS_INFO, PORT_PARTITION3, table, persons);
+        
+        
+    }
+
+    public static DBPartitionCommandGet sendPartitionInfo(int command, int port, String table, ArrayList<Person> persons) throws IOException {
         DBPartitionCommandPost postPartition;
         DBPartitionCommandGet get;
 
         socker_send_partition = new Socket("localhost", port);
-        postPartition = new DBPartitionCommandPost(getPartitionsInfo, table, persons);
+        postPartition = new DBPartitionCommandPost(command, table, persons);
         Connect.send(socker_send_partition, postPartition);
         get = (DBPartitionCommandGet) Connect.receive(socker_send_partition);
 
+        //Verificar se n da pau****************
+        socker_send_partition.close();
+        
         return get;
     }
 
